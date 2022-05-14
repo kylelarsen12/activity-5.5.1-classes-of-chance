1c06fe76092357b3b695785bf7c6df0d78255273
unknown <kylelarsen12@gmail.com>
2022-05-14 13:00:33 -0400

    Add app.js for tests & bonus tests

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 class Casino {
-    // Write code here
-};
+  // Write code here
+  constructor(name, isFakeCoin) {
+    this.name = name;
+    this.timesPlayed = 0;
+    this.isFakeCoin = isFakeCoin;
+  }
+
+  playGame(betAmount) {
+    this.timesPlayed++;
+    if (Math.random() <= 0.5 || this.isFakeCoin) {
+      console.log(`${this.name} wins, try again!`);
+    } else {
+      betAmount = betAmount * this.timesPlayed;
+      console.log(`Congratulations! You win ${betAmount}`);
+    }
+  }
+}
 
 // TESTS
 const myCasino = new Casino("HackerU Casino");
@@ -11,14 +26,13 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
+
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
 
 // Extra BONUS TESTS
 /*
